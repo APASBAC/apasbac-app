01baf63fc200032568e3f8f6dee8f1f523cb02a4
hhartur <hharturcarmello@gmail.com>
2026-09-17 21:06:20 -0300

    second commit

--- android/app/src/main/kotlin/com/example/apasbac_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.apasbac_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- assets/images/README.md
+++
@@ -1,1 +0,0 @@
-Adicione a logo aqui com o nome `apasbac_logo.png`. O app usa um ícone provisório enquanto a imagem não existe.
